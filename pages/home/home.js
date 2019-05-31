6f937f3cfb35f97d2f5f82729ebbb234f2f19189
yyyh1231 <444510796@qq.com>
2019-05-31 12:23:18 +0800

    搭建首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,11 +3,7 @@ Page({
 
   data: {
     lists:[
-      {id: 1, text:"111111111111111111", finished: true},
-      {id: 2, text:"222222222222222222", finished: true},
-      {id: 3, text:"333333333333333333", finished: false},
-      {id: 4, text:"444444444444444444", finished: false},
-      {id: 5, text:"555555555555555555", finished: false}
+  
     ]      
   },
   confirm(event){
