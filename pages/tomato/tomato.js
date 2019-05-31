089f7a05d3f1dfbb8323242e48098316da0b054f
yyyh1231 <444510796@qq.com>
2019-05-31 14:20:53 +0800

    设定时间

--- pages/tomato/tomato.js
+++ pages/tomato/tomato.js
@@ -1,66 +1,28 @@
 // pages/tomato/tomato.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    time: 1500,
+    xxx: ""
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  
   onShow: function () {
-
+    this.changeTime()
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  changeTime(){
+    let min = Math.floor(this.data.time/60)
+    let sec = Math.floor(this.data.time%60)
+    if(sec === 0){
+      sec = "00"
+    }
+    if((sec+"").length === 1){
+      sec = "0" + sec
+    }
+    if((min+"").length === 1){
+      min = "0" + min
+    }
+    this.setData({ xxx: `${min}:${sec}`})
   }
 })
