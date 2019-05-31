03692993e0a12b01ee792bb384d5e8b3a8d2029d
yyyh1231 <444510796@qq.com>
2019-05-31 17:46:01 +0800

    修复时间归零bug

--- pages/tomato/tomato.js
+++ pages/tomato/tomato.js
@@ -3,7 +3,7 @@ Page({
 
   timer: null,
   data: {
-    defaultSecond: 15,
+    defaultSecond: 5,
     time: "",
     timerStatus: "pause",
     confirmVisible: false,
@@ -19,12 +19,12 @@ Page({
     this.setData({timerStatus: "start"})
     this.changeTime()
     this.timer = setInterval(() => {
+      this.data.defaultSecond = this.data.defaultSecond - 1
+      this.changeTime()
       if(this.data.defaultSecond === 0){
         this.setData({againButtonVisible: true})
         return this.clearTimer()
-      }
-      this.data.defaultSecond = this.data.defaultSecond - 1
-      this.changeTime()
+      }      
     }, 1000)
   },
 
