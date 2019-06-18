4016ed95bf62d499a9e0ab3041080f9f257e2638
yyyh <444510796@qq.com>
2019-06-18 19:48:30 +0800

    gitignore

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',  //照抄
-    app_id: "wx027948f07d076c44",
-    app_secret: "1ddbc56b9ee294ae2b1f32c78292cd75",
-    t_app_id: "CJ4hgVh1F1z9AG2z9373rAiP",
-    t_app_secret: "v9sG4ufYMEG8W1ffUeHXLZaq"
-  }
-})
